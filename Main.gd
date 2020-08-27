cbda2cf6297fe3f87ea79de1a8535a616a08d2df
Logansummers18 <33754789+Logansummers18@users.noreply.github.com>
2020-08-26 20:50:55 -0400

    Add files via upload

--- Main.gd
+++ Main.gd
@@ -25,6 +25,8 @@ func game_over():
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -32,6 +34,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_MobTimer_timeout():
 	$MobPath/MobSpawnLocation.offset = randi()
